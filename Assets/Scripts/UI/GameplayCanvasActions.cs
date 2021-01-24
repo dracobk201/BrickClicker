dcb6035480cd51a342f5b35e0d637f1b1d8e409a
Erick Subero <suberoerick@gmail.com>
2021-01-24 02:16:27 -0400

    UI
    - Level name has been added to UI.

---
+++ Assets/Scripts/UI/GameplayCanvasActions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class GameplayCanvasActions : MonoBehaviour
+{
+    [SerializeField] private LevelRuntimeSet levels = null;
+    [SerializeField] private IntReference currentLevel = null;
+    [SerializeField] private TextMeshProUGUI levelLabel = null;
+
+    public void SetValues()
+    {
+        levelLabel.text = levels.Items[currentLevel.Value].levelName;
+    }
+}
